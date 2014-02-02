0163b1fbda28e0e1c27b16c87ba36b527fffea39
Berta-G <berta.gutierrez@gmail.com>
2014-02-02 04:04:42 +0000

    refactored Takeaway class and rspec

--- lib/Takeaway.rb
+++ lib/Takeaway.rb
@@ -5,22 +5,24 @@ class Takeaway
 
 	ACCOUNT_SID = "ACe3d5c93351424dc38b23ba6d175efb82"
 	AUTH_TOKEN = "c262a857500dada1189b61752ef2fc3e"
-	DEFAULT_WAITING_TIME = 3600
+	DEFAULT_WAITING_TIME = 3600 # in seconds
 
-	def initialize(options)
-		@menu = options[:dishes]
-		@messages_center = options[:messages_center]
-		@waiting_time = options.fetch([:waiting_time], DEFAULT_WAITING_TIME)
+	def initialize(initial_values = {})
+		@menu 				= initial_values.fetch(:menu, nil)
+		@sender				= initial_values.fetch(:sender, nil)
+		@waiting_time = DEFAULT_WAITING_TIME
 	end
 
-	attr_reader :menu, :messages_center, :waiting_time
+	attr_accessor :menu, :sender, :waiting_time
 
 	def list_menu
-		menu.map {|dish| dish.to_s}
+		menu.to_s
 	end
 
 	def confirm_order(order)
-		send_text if messages_center
+		valid_order = order.lines.count == order.total_lines 
+		raise "Incorrect order" if !valid_order
+		send_text if sender && order.phone_number
 	end
 
 	def text_body
@@ -33,7 +35,7 @@ class Takeaway
 	
 	private
 	def send_text
-		messages_center.send_text('+447720916582', text_body)
+		sender.send_text('+447720916582', text_body)
 	end
 	
 end
--- spec/Takeaway_spec.rb
+++ spec/Takeaway_spec.rb
@@ -1,45 +1,69 @@
 require './lib/Takeaway'
 
 describe "Takeaway" do 
-	let(:dish1) {double :dish}
-	let(:dish2) {double :dish}
-	let(:center) {double :messages_center}
-	let(:takeaway) {Takeaway.new({:dishes => [dish1, dish2], :messages_center => center})}
+	let(:menu) {double :menu}
+	let(:sender) {double :sender}
+	let(:takeaway) {Takeaway.new({:menu => menu, :sender => sender})}	
+	let(:takeaway_no_data) {Takeaway.new}
 	
 	context "Initialize" do
-		it "initializes the menu array from the aray dishes" do	
-			expect(takeaway.menu.size).to eq(2)
+
+		it "sets the waiting time to the default = 1 hour" do
+			expect(takeaway.waiting_time).to eq(3600)
 		end
 
-		it "assigns the message_center of the takeaway" do
-			expect(takeaway.messages_center). to eq(center)
+		it "sets to nil menu and sender if not passed" do
+			expect(takeaway_no_data.menu). to be(nil)
+			expect(takeaway_no_data.sender).to be(nil)
+		end			
+
+		it "can initialize the menu and the sender if passed" do	
+			expect(takeaway.menu).to eq(menu)
+			expect(takeaway.sender).to eq(sender)
 		end
 	end
 
-	it "lists all the dishes and prices in the menu" do
-			expect(dish1).to receive(:to_s).once
-			expect(dish2).to receive(:to_s).once
-			takeaway.list_menu
-	end
+	context "Functionalities" do
 
-	context "sending a text"  do 
-		
-		let(:time) 		{Time.new(2002, 10, 31, 2, 2, 2, "+02:00")}
-		let(:order) 	{double :order}
+		let(:line) {double :line}
+		let(:correct_order) {double :order, :total_lines => 1, :lines => [line], :phone_number => "0"}
+
+		it "lists the menu" do			
+				expect(takeaway.menu).to receive(:to_s).once
+				takeaway.list_menu
+		end
+
+		it "raises an error if the total of items is not correct on the order" do
+			wrong_order = double :order, :total_lines => 3, :lines => [line], :phone_number => "0"
+			expect{takeaway.confirm_order(wrong_order)}.to raise_error
+		end
+
+		it "can send a text when a correct order is received" do
+			expect(sender).to receive(:send_text)
+			takeaway.confirm_order(correct_order)
+		end
+
+		it "wont send a text if no receiver phone number is present" do
+			order_without_phone = double :order, :total_lines => 1, :lines => [line], :phone_number => nil
+			expect(sender).not_to receive(:send_text)
+			takeaway.confirm_order(order_without_phone)
+		end
+
+		it "wont send a text if no sender is present" do
+			expect(sender).not_to receive(:send_text)
+			takeaway_no_data.confirm_order(correct_order)
+		end
 
 		it "calculates the delivery time from the current time" do
+			time = Time.new(2002, 10, 31, 2, 2, 2, "+02:00")
 			expect(takeaway.delivery_time(time)).to eq("01:02")
 		end
+
 		it "creates the body of the text" do
 			Takeaway.any_instance.stub(:delivery_time => "23:05")
 			expect(takeaway.text_body).to eq("Your order has been received and it is being prepared. It will be delivered by 23:05")
 		end
-		it "calls the method send_text when an order arrives" do
-			expect(center).to receive(:send_text)
-			takeaway.confirm_order(order)
-		end
+	
 	end
-
-
-		
+	
 end
